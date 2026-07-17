cd20921bd02240b9b2534b64c5daf86cef49c322
Abner Silva <dylanabner14@gmail.com>
2026-07-17 01:03:31 -0600

    chore(database): add config files for set database conection

---
+++ src/database/database.provider.ts
@@ -0,0 +1,14 @@
+import * as mongoose from "mongoose";
+import { ConfigService } from "@nestjs/config";
+import { DATABASE_CONNECTION } from "src/constants";
+
+export const databaseProviders = [
+  {
+    provide: DATABASE_CONNECTION,
+    useFactory: (configService: ConfigService): Promise<typeof mongoose> => {
+      const mongoUri = configService.getOrThrow<string>("MONGODB_URI");
+      return mongoose.connect(mongoUri);
+    },
+    inject: [ConfigService],
+  },
+];
